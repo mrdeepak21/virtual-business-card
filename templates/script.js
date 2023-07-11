b01e5f7cb5853a2e7b402dae684cd406cec9a593
Reloaded <deepak@designandcode.tech>
2023-07-11 21:31:06 +0530

    vcf & dp

--- templates/script.js
+++ templates/script.js
@@ -5,15 +5,21 @@ function createVCard() {
   const email = document.getElementsByClassName('email')[0]!==undefined?document.getElementsByClassName('email')[0].innerHTML.trim():"";
   const website = document.getElementsByClassName('website')[0]!==undefined?document.getElementsByClassName('website')[0].innerHTML.trim():'';
   const address = document.getElementsByClassName('address')[0]!==undefined?document.getElementsByClassName('address')[0].innerHTML.trim():'';
+  const mobile = document.getElementsByClassName('mobile-data')[0]!==undefined?document.getElementsByClassName('mobile-data')[0].innerHTML.trim():'';
   const phone = document.getElementsByClassName('phone-data')[0]!==undefined?document.getElementsByClassName('phone-data')[0].innerHTML.trim():'';
+  const fax = document.getElementsByClassName('fax-data')[0]!==undefined?document.getElementsByClassName('fax-data')[0].innerHTML.trim():'';
   const linkedin = document.getElementsByClassName('linkedin')[0]!==undefined?document.getElementsByClassName('linkedin')[0].href.trim():"";
   const designation = document.getElementsByClassName('designation')[0]!==undefined?document.getElementsByClassName('designation')[0].innerHTML.trim():'';
+  const img = document.querySelector('.profile-photo img').src!==undefined? document.querySelector('.profile-photo img').src.trim():'';
+
+  const tel = mobile===""?phone:mobile;
 
   var vcardData = [
     'BEGIN:VCARD',
     'VERSION:3.0',
+    'PHOTO;VALUE#URI;TYPE#JPG:'+img,
     'N:' + user_name,
-    'TEL;TYPE=work,VOICE:' + phone,
+    'TEL;TYPE=work,VOICE:' + tel,
     'EMAIL:' + email,
     'ORG:' + 'Sterling Administration',
     'TITLE:' + designation,
