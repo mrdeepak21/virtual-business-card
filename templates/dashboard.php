db80412bfea5998b2bc192dd22a36c09ae8ad2e9
Reloaded <deepak@designandcode.tech>
2023-09-14 00:50:36 +0530

    change of pass folder

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -104,8 +104,8 @@ function createpng($src,$filename){
 }
 
 
-$outputDirectory = wp_upload_dir()['basedir'].'/virtual-business-cards/';
-$file_url = wp_upload_dir()['baseurl'].'/virtual-business-cards/';
+$outputDirectory = wp_upload_dir()['basedir'].'/digital-cards/';
+$file_url = wp_upload_dir()['baseurl'].'/digital-cards/';
 
 if(!file_exists($outputDirectory.$data.'.pkpass')) {
 if (!file_exists($outputDirectory)) {
